88fd0483c947536da7a4fa0f3f9e3f2acc0beaf0
Shrilaxmi vijay Joshi <svj28@afsconnect1.njit.edu>
2017-12-17 16:18:30 -0500

    hmpg

--- pages/homepage.php
+++ pages/homepage.php
@@ -1,6 +1,4 @@
 <html lang="en">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-
 <head>
     <meta charset="utf-8">
 
@@ -18,30 +16,33 @@
 <body>
 
 
-<h1>
+<h2>
     <?php
     //this how to print some data;
     echo $data['site_name'];
-    ?> </h1>
+    ?> </h2>
 
-<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
-<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
+<h3><a href="index.php?page=accounts&action=all">Show All Accounts</a></h3>
+<h3><a href="index.php?page=tasks&action=all">Show All Tasks</a></h3>
 
-<form action="index.php?page=accounts&action=login" method="POST" align='center'>
+<form action="index.php?page=accounts&action=login" method="POST">
 
-    <div class="container" align='left'>
+    <div class="container">
         <label><b>Username</b></label>
         <input type="text" placeholder="Enter Username" name="email" required><br>
 
         <label><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required><br>
+        <input type="password" placeholder="Enter Password" name="pass" required>
+        <br>
 
         <button type="submit">Login</button>
     </div>
+    </div>
+
 
 
 </form>
-<h1><a href="index.php?page=accounts&action=register">Register</a></h1>
+<h3><a href="index.php?page=accounts&action=register">New User?</a></h3>
 
 
 <script src="js/scripts.js"></script>

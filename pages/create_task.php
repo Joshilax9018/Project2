0be29cb248a2f63f96e2296c2fd0b79f3445911a
Shrilaxmi vijay Joshi <svj28@afsconnect1.njit.edu>
2017-12-19 02:15:12 -0500

    create

--- pages/create_task.php
+++ pages/create_task.php
@@ -1,34 +1,36 @@
+<!doctype html>
+
 <html lang="en">
 <head>
-        <meta charset="utf-8">
+    <meta charset="utf-8">
 
-        <title>The HTML5 Herald</title>
-        <meta name="description" content="The HTML5 Herald">
-        <meta name="author" content="SitePoint">
+    <title>New Task</title>
+    <meta name="description" content="The HTML5 Herald">
+    <meta name="author" content="SitePoint">
 
-        <!--<link rel="stylesheet" href="css/styles.css?v=1.0">-->
+    <!--<link rel="stylesheet" href="css/styles.css?v=1.0">-->
 
-        <!--[if lt IE 9]>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-        <link rel="stylesheet" href="css/style.css">
-        <![endif]-->
-    </head>
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <![endif]-->
+</head>
 
 <body>
 
 
-<form action="index.php?page=tasks&action=create" method="post">
-        Owner Email: <input type="text" name="owneremail" value="<?php echo $data->owneremail ?>"><br/>
-        Owner ID: <input type="text" name="ownerid" value="<?php echo $data->ownerid ?>"><br/>
-        Create Date: <input type="text" name="createddate"><br/>
-        Due Date: <input type="text" name="duedate"><br/>
-        Message: <input type="text" name="message"><br/>
-        Is Done: <input type="text" name="isdone"><br/><br>
-
-        <button type="submit" id="create">Add</button>
-    </form>
-
+<form action="index.php?page=tasks&action=addTask" method="post">
+    Owner Email: <input type="text" name="owneremail"><br/>
+    Owner ID: <input type="text" name="ownerid"><br/>
+    Create Date: <input type="text" name="createddate"><br/>
+    Due Date: <input type="text" name="duedate"><br/>
+    Message: <input type="text" name="message"><br/>
+    Is Done: <input type="text" name="isdone"><br/><br>
+    <input type="submit" value="Add Task">
+    
+</form>
+<a href="index.php?page=tasks&action=all">Back to all tasks</a>
 
 <script src="js/scripts.js"></script>
 </body>

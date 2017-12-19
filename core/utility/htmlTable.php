a211355ba95e5ab9fa30a43a3c5321e5dda36a08
Shrilaxmi vijay Joshi <svj28@afsconnect1.njit.edu>
2017-12-19 02:30:27 -0500

    changes

--- core/utility/htmlTable.php
+++ core/utility/htmlTable.php
@@ -3,7 +3,7 @@ namespace utility;
 //namespace MyProject\mvcName;
 class htmlTable
 {
-    public static function genarateTableFromMultiArray($array)
+    /*public static function genarateTableFromMultiArray($array)
     {
         $tableGen = '<table border="1"cellpadding="10">';
         //this grabs the first element of the array so we can extract the field headings for the table
@@ -37,6 +37,45 @@ class htmlTable
             }
         }
         
+        $tableGen .= '</table>';
+        return $tableGen;
+    } */
+    
+    public static function genarateTableFromMultiArray($array)
+    {
+        $tableGen = '<table class="table-hover align-items-center table table-bordered">';
+        $tableGen .= '<tr class="active">';
+        //this grabs the first element of the array so we can extract the field headings for the table
+        $fieldHeadings = $array[0];
+        $fieldHeadings = get_object_vars($fieldHeadings);
+        $fieldHeadings = array_keys($fieldHeadings);
+        //this gets the page being viewed so that the table routes requests to the correct controller
+        $referingPage = $_REQUEST['page'];
+        foreach ($fieldHeadings as $heading) 
+        {
+            $tableGen .= '<th>' . $heading . '</th>';
+        }
+        $tableGen .= '</tr>';
+        foreach ($array as $record) 
+        {
+            $tableGen .= '<tr class="active">';
+            foreach ($record as $key => $value) 
+            {
+                if ($key == 'id') 
+                {
+                    $tableGen .= '<td><a href="index.php?page=' . $referingPage . '&action=show&id=' . $value . '">View</a></td>';
+                } 
+                else 
+                {
+                    $tableGen .= '<td>' . $value . '</td>';
+                }
+            }
+            
+                /*$tableGen .= '<td><a href="index.php?page=' . $referingPage . '&action=edit&id=' . $id . '">Edit</a></td>';
+                $tableGen .= '<td><a href="index.php?page=' . $referingPage . '&action=delete&id=' . $id . '">Delete</a></td>'; */
+             
+            $tableGen .= '</tr>';
+        }
         $tableGen .= '</table>';
         return $tableGen;
     }
